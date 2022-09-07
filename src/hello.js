ba468a0fad96576ef1e0636837ceeb51ac975ed5
ofer affias <ofer.affias@gmail.com>
2022-09-07 15:19:24 +0300

    fmt

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,6 @@
-var http = require('http');
+var http = require( 'http' );
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Hello World!');
+}).
+listen(8080);
