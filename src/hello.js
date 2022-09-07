d1d4b933e94c239a1e8da9966c40a2b69662a85e
ofer affias <ofer.affias@gmail.com>
2022-09-07 16:46:47 +0300

    short lije

--- src/hello.js
+++ src/hello.js
@@ -2,6 +2,8 @@ var http = require( 'http' );
 
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Hello World!');
+    res.writeHead(200, {'Content-Type': 'text/plain'}); 
+
+    res.end('Hello World!');
 }).
 listen(8080);
