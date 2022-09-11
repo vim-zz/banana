b939ca6d766cb3d902dab61abec3f4c3fc36dcbf
ofer affias <ofer.affias@gmail.com>
2022-09-11 14:04:09 +0300

    fmt

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
-const http = require('http');
+const http    = require('http');
 
-const hostname = '127.0.0.1';
+const hostname =    '127.0.0.1';
 const port = 3000;
+
+
+
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  const txt = "hello";
-  const reg = /hello/;
+  const txt = "hello"; const reg = /hello/;
   const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
   console.log(x);
   res.end("hi");
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(
+    `Server running at http://${hostname}:${port}/`);
 });
