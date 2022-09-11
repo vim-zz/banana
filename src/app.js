71e4ccf52cad8ea37b70c1362b7175fbd6eddd49
ofer affias <ofer.affias@gmail.com>
2022-09-11 14:05:41 +0300

    s

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/plain'); 
 
   const txt = "hello"; const reg = /hello/;
   const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
