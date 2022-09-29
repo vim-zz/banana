d115c623217a1b0d034f042abfd0b5623d67e893
ofer affias <ofer.affias@gmail.com>
2022-09-29 11:41:37 +0300

    only added console

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
+  console.log("ok")
   const txt = "hello";
   const reg = /hello/;
   const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
