0ceabb13d8072e1ce0ee73f2f4760e73db29aae4
ofer affias <ofer.affias@gmail.com>
2022-09-29 11:52:26 +0300

    added console and BUG

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  console.log("ok")
 
+
+  bamamam bu gug bug :x
   const txt = "hello";
   const reg = /hello/;
   const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
