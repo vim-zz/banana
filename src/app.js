d5e3d72369412e60844dc8502acaa98b311ca8bf
ofer affias <ofer.affias@gmail.com>
2022-09-29 09:04:56 +0300

    single

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  console.log(x);
 
   const txt = "hello";
   const reg = /hello/;
