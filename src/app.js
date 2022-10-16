cdd2d6092f830653e6903cf94b6363778effc315
Ofer Affias <ofer.affias@gmail.com>
2022-10-16 17:41:24 +0300

    Update app.js
    very large change

--- src/app.js
+++ src/app.js
@@ -15,6 +15,357 @@ const server = http.createServer((req, res) => {
   res.end("hi");
 });
 
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+v
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+v// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+// hello 1
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
