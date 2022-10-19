7781ce003ee9fce65265cd8aaf03680a95380dc0
ofer affias <ofer.affias@gmail.com>
2022-10-19 19:14:51 +0300

    mish

--- src/hello.js
+++ src/hello.js
@@ -5,4 +5,5 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'}); 
   res.end('Hello World!');
 })
+self.assertEqual (event ["detail-type"], Types.SCAN_FAILED);
 .listen(8080);
