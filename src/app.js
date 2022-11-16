000fce0228a3b966c366fac82c72762498ee96dd
ofer affias <ofer.affias@gmail.com>
2022-11-16 11:50:48 +0200

    sum stuff

--- src/app.js
+++ src/app.js
@@ -15,6 +15,19 @@ const server = http.createServer((req, res) => {
   res.end("hi");
 });
 
+// hello 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  const txt = "hello";
+  const reg = /hello/;
+  const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
+  console.log(x);
+  res.end("hi");
+});
+
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
