a3d5e5293813dd9d03ce542ae04339d120c932be
ofer affias <ofer.affias@gmail.com>
2022-11-21 16:48:28 +0200

    sum stuff

--- src/app.js
+++ src/app.js
@@ -8,8 +8,6 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  const txt = "hello";
-  const reg = /hello/;
   const x = `${txt} is ${typeof txt} ${reg} is ${typeof reg}`;
   console.log(x);
   res.end("hi");
