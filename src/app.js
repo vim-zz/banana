b114e0b1d78554da6c13d16444f9552c769f9be3
Ofer Affias <ofer.affias@gmail.com>
2023-12-19 15:25:27 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const port = 3000;
 // hello 
 const server = http.createServer((req, res) => {
   res.statusCode = 20; 
-  res.end("hi");
+  res.end("hi") // lalal
 });
 
 server.listen(port, hostname, () => {
