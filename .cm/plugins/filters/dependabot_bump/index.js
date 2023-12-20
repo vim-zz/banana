324f6e25e17d33c8136ecfd9cef161ea5d26433f
ofer affias <ofer.affias@gmail.com>
2023-12-20 18:46:36 +0200

    ok

--- .cm/plugins/filters/dependabot_bump/index.js
+++ .cm/plugins/filters/dependabot_bump/index.js
@@ -1,9 +1,13 @@
 module.exports = (desc) => {
-  var [_, from, to] = /Bumps.*from ([\d\.]+[A-Za-zαß]*) to ([\d\.]+[A-Za-zαß]*)/.exec(desc);
-  // remove trailing dot on to
-  if (to[to.length - 1] === ".") {
-    to = to.slice(0, -1);
-  }
+  if (desc) {    
+    var [_, from, to] = /Bumps.*from ([\d\.]+[A-Za-zαß]*) to ([\d\.]+[A-Za-zαß]*)/.exec(desc);
+    // remove trailing dot on to
+    if (to[to.length - 1] === ".") {
+      to = to.slice(0, -1);
+    }
 
-  return [to, from];
+    return [to, from];
+  }
+  
+  return null;
 }
