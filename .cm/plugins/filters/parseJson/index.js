5fdb6870796fa96a7928e699a0f11eb1e8124551
ofer affias <ofer.affias@gmail.com>
2023-12-28 16:49:15 +0200

    ok

--- .cm/plugins/filters/parseJson/index.js
+++ .cm/plugins/filters/parseJson/index.js
@@ -1,5 +1,5 @@
 module.exports = (json) => {
   console.log("JSON", {json});
   console.log("JSON PARSE", JSON.parse(json));
-  return JSON.parse(json);
+  return JSON.stringify(JSON.parse(json));
 }
