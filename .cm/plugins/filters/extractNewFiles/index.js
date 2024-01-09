21392e03e853689cc4f119953b66459f1a84bc07
ofer affias <ofer.affias@gmail.com>
2024-01-09 12:58:04 +0200

    just print

--- .cm/plugins/filters/extractNewFiles/index.js
+++ .cm/plugins/filters/extractNewFiles/index.js
@@ -1,22 +1,22 @@
 function extractNewFiles(subObjects, fileType) {
     console.log("Identifying new files...", {subObjects, fileType})
 
-    const filteredSubObjects = {};
+    // const filteredSubObjects = {};
 
-    for (const [plugin, objects] of Object.entries(subObjects)) {
-        const hasIndexJS = objects.some((subObject) => subObject.new_file.includes(fileType) && subObject.original_file === '');
+    // for (const [plugin, objects] of Object.entries(subObjects)) {
+    //     const hasIndexJS = objects.some((subObject) => subObject.new_file.includes(fileType) && subObject.original_file === '');
     
-        if (hasIndexJS) {
-            filteredSubObjects[plugin] = objects;
-        } else {
-            filteredSubObjects[plugin] = [];
-        }
-    }
+    //     if (hasIndexJS) {
+    //         filteredSubObjects[plugin] = objects;
+    //     } else {
+    //         filteredSubObjects[plugin] = [];
+    //     }
+    // }
 
-    for (const k in filteredSubObjects) {
-        if (Array.isArray(filteredSubObjects[k]) && filteredSubObjects[k].length === 0) {
-            delete filteredSubObjects[k];
-        }
-    }
-    return filteredSubObjects;
+    // for (const k in filteredSubObjects) {
+    //     if (Array.isArray(filteredSubObjects[k]) && filteredSubObjects[k].length === 0) {
+    //         delete filteredSubObjects[k];
+    //     }
+    // }
+    // return filteredSubObjects;
 }
