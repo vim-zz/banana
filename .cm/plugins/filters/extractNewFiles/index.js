000146e52e1622ef9c9562fd664643d792840efe
ofer affias <ofer.affias@gmail.com>
2024-02-07 16:55:20 -0500

    fix

--- .cm/plugins/filters/extractNewFiles/index.js
+++ .cm/plugins/filters/extractNewFiles/index.js
@@ -1,7 +1,7 @@
 function extractNewFiles(fileDiffs, fileTypeRegexStr) {
     // console.log("Identifying new files...", {fileDiffs, fileTypeRegexStr})
     const fileTypeRegex = new RegExp(fileTypeRegexStr);
-    const newFiles = src
+    const newFiles = fileDiffs
       .filter(item => item.original_file === "")
       .filter(item => fileTypeRegex.test(item.new_file))
       .map(item => item.new_file);
