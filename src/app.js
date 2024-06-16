09e85fdddc9d3ae13651cba7bcdcb39bc59b5cb0
Ofer Affias <ofer.affias@gmail.com>
2024-06-16 13:56:26 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,11 +12,6 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
-  res.end("hi");
 });
 // hello 
 const server = http.createServer((req, res) => {
