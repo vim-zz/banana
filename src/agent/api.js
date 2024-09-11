3979632ec79624444b58b3fe3703db4fdf4e4d59
Ofer Affias <ofer.affias@gmail.com>
2024-09-11 18:51:10 -0400

    Apply changes from diff

--- src/agent/api.js
+++ src/agent/api.js
@@ -1,3 +1,11 @@
+a/src/agent/api.js b/src/agent/api.js
+index 2b22260b..b9c05787 100644
+--- a/src/agent/api.js
++++ b/src/agent/api.js
+@@ -1,3 +1,6 @@
 // new api service handler
-// chane API signature
+// change API signature
 
++function printHello() {
++    console.log("Hello");
++}
