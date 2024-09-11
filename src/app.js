8d28979f7a228fae181eb2e89a80562d0070ba65
Ofer Affias <ofer.affias@gmail.com>
2024-09-11 19:32:19 -0400

    Apply changes from diff

--- src/app.js
+++ src/app.js
@@ -1,30 +1,45 @@
-const http = require('http');
+a/src/app.js b/src/app.js
+index 44de9904..7987df67 100644
+--- a/src/app.js
++++ b/src/app.js
+@@ -3,28 +3,24 @@ const http = require('http');
+ const hostname = '127.0.0.1';
+ const port = 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
+-// hello 
+-const server = http.createServer((req, res) => {
+-  res.statusCode = 20; 
++// hello
++const server0 = http.createServer((req, res) => {
++  res.statusCode = 20;
+   res.end("hi");
+ });
 
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
-  res.end("hi");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
-  res.end("hi");
-});
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
-  res.end("hi");
-});
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
-  res.end("hi");
-});
+-server.listen(port, hostname, () => {
+-  console.log(`Server running at http://${hostname}:${port}/`);
+-});
+-
+-// hello 
+-const server = http.createServer((req, res) => {
+-  res.statusCode = 20; 
++// hello
++const server1 = http.createServer((req, res) => {
++  res.statusCode = 20;
+   res.end("hi");
+ });
+-// hello 
+-const server = http.createServer((req, res) => {
+-  res.statusCode = 20; 
++// hello
++const server2 = http.createServer((req, res) => {
++  res.statusCode = 20;
+   res.end("hi");
+ });
+-// hello 
+-const server = http.createServer((req, res) => {
+-  res.statusCode = 20; 
++// hello
++const server3 = http.createServer((req, res) => {
++  res.statusCode = 20;
+   res.end("hi");
+ });
