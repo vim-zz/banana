9827aa3c3f2109f00f3b7d826dbf8acef63b8fa7
Ofer Affias <ofer.affias@gmail.com>
2024-09-13 15:38:02 -0400

    Apply changes from diff

--- src/app.js
+++ src/app.js
@@ -1,30 +1,21 @@
-const http = require('http');
+a/src/app.js b/src/app.js
+index 44de9904..2c1abd78 100644
+--- a/src/app.js
++++ b/src/app.js
+@@ -10,18 +10,16 @@ const port = 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
+// hello
+const server2 = http.createServer((req, res) => {
+  res.statusCode = 20;
   res.end("hi");
 });
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
+// hello
+const server3 = http.createServer((req, res) => {
+  res.statusCode = 20;
   res.end("hi");
 });
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
-  res.end("hi");
-});
-// hello 
-const server = http.createServer((req, res) => {
-  res.statusCode = 20; 
+// hello
+const server4 = http.createServer((req, res) => {
+  res.statusCode = 20;
   res.end("hi");
 });
