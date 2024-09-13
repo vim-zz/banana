071bc17243c6e9710c708c424261fd834f7ae956
Ofer Affias <ofer.affias@gmail.com>
2024-09-13 17:53:50 -0400

    Apply changes from diff

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-# mango
+a/setup.py b/setup.py
+index f4ddf6ec..5c0d9bb3 100644
+--- a/setup.py
++++ b/setup.py
+@@ -1,11 +1,5 @@
+ # mango
 
-
-# sadas
-# banana
-# dsa
-# sadas
-
-def function():
+-
+-# sadas
+-# banana
+-# dsa
+-# sadas
+-
+ def function():
 	print('1')
 	pass
